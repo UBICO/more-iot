fd271dc43671bccd100a3580d08dac09bc950049
Luca Bedogni <luca.bedogni@unimore.it>
2022-06-08 16:08:49 +0200

    01-02-sensor

---
+++ exercises/01-02-sensor/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+//KY018 Photo resistor module
+ 
+int sensorPin = 2; // select the input pin for the potentiometer
+int sensorValue = 0; // variable to store the value coming from the sensor
+int LED = 2;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  
+  if (sensorValue < 200) {
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+
+  Serial.println(sensorValue, DEC);
+  delay(2000);
+}
