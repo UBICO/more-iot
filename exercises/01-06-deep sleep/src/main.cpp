3a7bb38f4015865d30abbf7dbf1c478c6c4500a9
Luca Bedogni <luca.bedogni@unimore.it>
2022-06-23 16:22:45 +0200

    01-065 deep sleep

---
+++ exercises/01-06-deep sleep/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#define MILLIS 1000000
+#define TIME_TO_SLEEP 30
+
+RTC_DATA_ATTR int boot = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(50);
+  boot++;
+
+  Serial.println("Boot number " + String(boot));
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * MILLIS);
+
+  Serial.println("Sleeping for " + String(TIME_TO_SLEEP) + " seconds");
+  Serial.println("Sleep start.");
+  delay(10);
+  Serial.flush();
+
+  esp_deep_sleep_start();
+}
+
+void loop() {}
